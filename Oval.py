b3568a044645aa953999f50910ae8cf41dc9d54a
Liam Rogers <LiamRogersUK@gmail.com>
2024-06-19 16:34:04 +0100

    Create Oval.py
    This code draws an oval. For some reason, having the turtle hide causes the oval to disappear.

---
+++ Oval.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+#Initialise turtle
+speed(0)
+bgcolor("magenta")
+
+#Draw oval
+shape("circle")
+fillcolor("white")
+shapesize(30, 20, 5) #(length, width, outline)
+
+exitonclick()
