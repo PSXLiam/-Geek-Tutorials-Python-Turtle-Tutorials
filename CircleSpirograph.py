e88f13d7f61331cca25f93f57809f7a2e1acda93
Liam Rogers <LiamRogersUK@gmail.com>
2024-06-20 15:32:40 +0100

    Implement variables & while loop
    List moved to variable. Variable for angle added. Outer for loop changed to a while loop that uses a variable that monitors if the pattern has completed a full rotation

--- CircleSpirograph.py
+++ CircleSpirograph.py
@@ -5,14 +5,20 @@ speed(0)
 bgcolor("black")
 pensize(2)
 
-#for each set of circles
-for i in range(6):
+#Define variables for code
+colourRange = ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]
+angle = 10
+fullrotation = 0
+
+#while the pattern has not made a full rotation
+while fullrotation <= 360:
     #for each colour
-    for colours in ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]:
-        #draw circle of current color then rotate 10° to left
+    for colours in colourRange:
+        #draw circle of current colour then rotate 10° to left
         color(colours)
         circle(100)
         left(10)
+        fullrotation = fullrotation + angle # increase value of 'fullrotation' by angle moved
 
 #Hide turtle
 hideturtle()
