801b03c074be7c1d4942a1cf542392438ea9200b
Liam Rogers <LiamRogersUK@gmail.com>
2024-06-20 16:36:44 +0100

    Add triangle sections to logo
    The triangular parts of the logo are added, I remember making this pattern before when I created OctagonPattern.py

--- FalloutShelterLogo.py
+++ FalloutShelterLogo.py
@@ -13,6 +13,19 @@ begin_fill()
 circle(225)
 end_fill()
 
+#Triangle part
+penup()
+goto(0, 0)
+pendown()
+color("gold")
+begin_fill()
+for i in range(3):
+    left(120)
+    for i in range(3):
+        forward(200)
+        left(120)
+end_fill()
+
 #Hide turtle
 hideturtle()
 
