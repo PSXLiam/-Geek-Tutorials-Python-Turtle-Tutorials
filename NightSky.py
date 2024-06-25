dcc34301639b522cbca0824eb801a69b636039b8
Liam Rogers <LiamRogersUK@gmail.com>
2024-06-25 14:22:38 +0100

    Create Make Star Function
    A function is added and called once to draw a single star shape to the window.

--- NightSky.py
+++ NightSky.py
@@ -5,4 +5,18 @@ setup(800, 500)
 speed(0)
 bgcolor("midnight blue")
 
+#Make Star Function
+def make_star():
+    color("yellow")
+    begin_fill()
+    for i in range(5):
+        forward(5)
+        left(72)
+        forward(5)
+        right(144)
+    end_fill()
+
+#Draw Star
+make_star()
+
 exitonclick()
